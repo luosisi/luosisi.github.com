9f300de090e5d9d2b2fccb29bdfcc974244d5300
luosisi <478066577@qq.com>
2016-04-19 18:11:02 +0800

    js
    js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Admin on 2016/3/15.
+ */
+$(function(){
+    $(".contact-back").click(function(){
+        location="#top";
+    })
+   /* console.log($("#topimg").height())*/
+})
+
